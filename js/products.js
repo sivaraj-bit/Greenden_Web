c39ed217d7b02217cbcf33eeb12930911d89c53a
sivaraj-bit <sivagovindharaj33@gmail.com>
2026-09-17 16:25:37 +0530

    added filter function

--- js/products.js
+++ js/products.js
@@ -11,3 +11,34 @@ mobileBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
     mobileMenu.classList.add("hidden");
 });
+
+//filter products
+
+const productsContainer = document.getElementById("product-container")
+const search = document.getElementById("search")
+const productList = productsContainer.querySelectorAll("div")
+
+
+search.addEventListener("keyup",function(){
+    const enteredVAlue = event.target.value.toUpperCase()
+
+    for(count=0;count<productList.length;count=count+1)
+    {
+        const productName = productList[count].querySelector("h3").textContent
+
+        if(productName.toUpperCase().indexOf(enteredVAlue)<0)
+        {
+            productList[count].style.display="none"
+        }
+        else
+        {
+            productList[count].style.display="block"
+        
+        }
+    }
+
+
+
+
+
+})
